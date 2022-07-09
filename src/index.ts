06be265c62c16c092e2ee7b0eb09a2644c2323f1
masaya0521 <kaji0521km@gmail.com>
2022-07-10 01:08:48 +0900

    bigIntの確認

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,18 @@
 const message: string = "Hello world";
 
-console.log(message);
+const NumberStartTime: number = Date.now();
+let coutNumber: number = 0;
+for (let index = 0; index < 100; index++) {
+  coutNumber = 1 + 1;
+}
+const NumberEndTime = Date.now(); // 終了時間
+
+const BigIntStartTime: number = Date.now();
+let coutBigInt: BigInt = 0n;
+for (let index = 0; index < 100; index++) {
+  coutBigInt = 1n + 1n;
+}
+const BigIntEndTime = Date.now(); // 終了時間
+
+console.log(`numberの処理時間 : ${NumberEndTime - NumberStartTime} ミリ秒`); // 10000000000のforでまわして9252ミリ秒
+console.log(`bigIntの処理時間 : ${BigIntEndTime - BigIntStartTime} ミリ秒`); // 10000000000のforでまわして9238ミリ秒
