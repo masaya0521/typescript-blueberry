7934c0ecc4f2cfed00a60b44fbf9b71f251c050e
masaya0521 <kaji0521km@gmail.com>
2022-07-13 22:56:14 +0900

    アロー関数

--- src/type.ts
+++ src/type.ts
@@ -6,3 +6,20 @@ type Family<name extends Human, Child extends Human> = {
   mother: name;
   father: Child;
 };
+
+const obj = {
+  name: "name",
+  email: "test",
+};
+
+// アロー関数ds
+const sum = (...args: number[]): number => {
+  let result = 0;
+  for (const num of args) {
+    result += num;
+  }
+  return result;
+};
+
+console.log(sum(1, 10, 100));
+console.log(sum(1, 5));
